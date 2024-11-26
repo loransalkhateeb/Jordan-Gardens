5235ddce86a5d089aeb1d2b2e1b3952feb497ac8
loransalkhateeb <lwrnsalkhtyb9@gmail.com>
2024-11-26 16:28:19 +0300

    Updated On BackEnd

--- Config/multer.js
+++ Config/multer.js
@@ -2,11 +2,11 @@ const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 
-
 const customStorage = multer.diskStorage({
   destination: (req, file, cb) => {
     const uploadPath = "uploads/";
 
+    
     if (!fs.existsSync(uploadPath)) {
       fs.mkdirSync(uploadPath, { recursive: true });
     }
@@ -16,9 +16,7 @@ const customStorage = multer.diskStorage({
     const filename = file.originalname;
     const filePath = path.join("uploads", filename);
 
-
     if (fs.existsSync(filePath)) {
-    
       const timestamp = Date.now();
       const ext = path.extname(filename);
       const baseName = path.basename(filename, ext);
@@ -31,22 +29,13 @@ const customStorage = multer.diskStorage({
 
 
 const fileFilter = (req, file, cb) => {
-  const allowedTypes = /jpg|jpeg|png|gif/; 
-  const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = allowedTypes.test(file.mimetype);
-
-  if (extname && mimetype) {
-    cb(null, true);
-  } else {
-    cb(new Error("Only images are allowed"), false);
-  }
+  cb(null, true);
 };
-
 
 const upload = multer({
   storage: customStorage,
-  fileFilter,
-  limits: { fileSize: 1024 * 1024 * 5 }, 
+  fileFilter, 
+  limits: { fileSize: 1024 * 1024 * 10 }, 
 });
 
 module.exports = upload;
